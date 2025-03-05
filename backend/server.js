a2c0fea2d6e828b1429ceded204d8a5d1e6e3997
MohamedHany17m8 <mhany6921@gmail.com>
2025-03-06 01:50:40 +0200

    coupon

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import connectDB from "./lib/db.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 dotenv.config();
 connectDB();
 
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
