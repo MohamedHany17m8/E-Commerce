483a39db6e3548b90da546e19cf81f60ba603fa6
MohamedHany17m8 <mhany6921@gmail.com>
2025-03-06 01:24:34 +0200

    cart

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import connectDB from "./lib/db.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 dotenv.config();
 connectDB();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
