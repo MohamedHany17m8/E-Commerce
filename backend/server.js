00e5c673ca504d8fb2d8a76eb9e3289cc2498850
MohamedHany17m8 <mhany6921@gmail.com>
2025-03-05 22:16:36 +0200

    auth & products

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import connectDB from "./lib/db.js";
-
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 connectDB();
 
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
