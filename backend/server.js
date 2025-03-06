b5c5d8e2420eff3a4b1931ea71e9b9dfd2b3a77f
MohamedHany17m8 <mhany6921@gmail.com>
2025-03-06 15:40:05 +0200

    analytics done

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 dotenv.config();
 connectDB();
 
@@ -18,6 +19,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
