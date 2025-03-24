ed900e028ab233b60944eebf4aa4c48b17380670
MohamedHany17m8 <mhany6921@gmail.com>
2025-03-24 17:34:05 +0200

    vercel

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,9 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
